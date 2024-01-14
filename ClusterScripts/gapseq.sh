4ec7bc9f4c6e5f4d82638c351cb54f94b857924e
SteFlor <stefano.flor1994@gmail.com>
2024-01-14 14:32:18 +0100

    restructured git folders and keep-ignore policies to facilitate use

--- ClusterScripts/gapseq.sh
+++ ClusterScripts/gapseq.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-#SBATCH --array 0-63
-#SBATCH --job-name=gapseqFinalMAGs-%A
-#SBATCH --error=gapseqFinalMAGs-%A.err
-#SBATCH --output=gapseqFinalMAGs-%A.out
+
+#SBATCH --array 0-3616
+#SBATCH --job-name=gap
+#SBATCH --error=error_log/gap_%A_%a.err
+#SBATCH --output=output_log/gap_%A_%a.out
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=1-23:59:00
 #SBATCH --mem=64G
-#SBATCH --mail-type=ALL
 
+
+echo "starting"
 source ~/.bashrc
 micromamba activate gapseq_env
-files=(FinalMAGs/*.fna)
+cd ../AllModels
+files=(../Genomes/*.fna)
 file="${files[$SLURM_ARRAY_TASK_ID]}"
 echo $file
 
